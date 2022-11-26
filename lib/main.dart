38984cc5dc5bde963341e6da4c3d2653ac01a5e7
engcraft1102 <engcraft1102@gmail.com>
2022-11-26 23:40:16 +0900

    bottom nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -15,18 +15,36 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Instagram',
-          ),
-          actions: [
-            IconButton(
-              icon: Icon(Icons.add_box_outlined),
-              onPressed: () {},
-            )
-          ],
+      appBar: AppBar(
+        title: Text(
+          'Instagram',
         ),
-        body:
-          );
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add_box_outlined),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Text(''),
+      bottomNavigationBar: BottomNavigationBar(
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home_outlined), label: '홈'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.shopping_bag_outlined), label: '샵')
+          ]),
+    );
+  }
+}
+
+class InstaBottomNav extends StatelessWidget {
+  const InstaBottomNav({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
--- lib/style.dart
+++ lib/style.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 
-var _privateVar;
-
 var theme = ThemeData(
-    appBarTheme: AppBarTheme(
-        color: Colors.white,
-        centerTitle: false,
-        elevation: 1,
-        titleTextStyle: TextStyle(color: Colors.black, fontSize: 25),
-        actionsIconTheme: IconThemeData(color: Colors.black, size: 30)));
+  appBarTheme: AppBarTheme(
+      color: Colors.white,
+      centerTitle: false,
+      elevation: 1,
+      titleTextStyle: TextStyle(color: Colors.black, fontSize: 25),
+      actionsIconTheme: IconThemeData(color: Colors.black, size: 30)),
+  bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      selectedItemColor: Colors.black, elevation: 2),
+);
