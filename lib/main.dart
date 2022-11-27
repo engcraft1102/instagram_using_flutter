3166a065cdc9ff3aee104d5d6d3f970b7315105d
engcraft1102 <engcraft1102@gmail.com>
2022-11-27 15:15:03 +0900

    TabView

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,15 @@ void main() {
   ));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int currentTab = 0;
 
   // This widget is the root of your application.
   @override
@@ -26,10 +33,15 @@ class MyApp extends StatelessWidget {
           )
         ],
       ),
-      body: Text(''),
+      body: [Text('홈'), Text('샵')][currentTab],
       bottomNavigationBar: BottomNavigationBar(
           showSelectedLabels: false,
           showUnselectedLabels: false,
+          onTap: (value) => {
+                setState(() {
+                  currentTab = value;
+                })
+              },
           items: [
             BottomNavigationBarItem(
                 icon: Icon(Icons.home_outlined), label: '홈'),
